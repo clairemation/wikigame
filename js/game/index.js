8dc8362ccd63a9ae71b5c62f6bcf91195dae1951
claire <clairemation@gmail.com>
2025-10-05 07:42:27 -0400

    blah

--- js/game/index.js
+++ js/game/index.js
@@ -1,6 +1,7 @@
 const nmg = require('node-maze-generator');
 const {getArticleProperties} = require('../wiki-api/midlevelmanager.mjs');
 
+const linkInfoParent = document.querySelector('#linkinfo')
 const ctx = document.querySelector('canvas').getContext('2d');
 
 const CELL_WIDTH = 60, CELL_HEIGHT = 60;
@@ -11,18 +12,27 @@ let directionX = 0, directionY = 0;
 let windowX = 0, windowY = 0;
 let speed = 0.05;
 let keyStatus = {}
-let deltaTime = 0;
 let playerX = 0, playerY = 0;
+let exitsAreOpen = false;
 
 start();
 
+async function start()
+{
+  addEventListener("keydown", e => keyStatus[e.key] = true);
+  addEventListener("keyup", e => keyStatus[e.key] = false);
+
+  addEventListener("mousedown", processMouseClick);
+
+  // const articleProperties = await getArticleProperties("bassoon");
+  // const mazeProperties = generateMazeProperties(articleProperties);
+  const mazeProperties = {size: 20, simplicity: 0.6, links: [{}, {}, {}, {}, {}]}
+  setupMaze(mazeProperties);
+  requestAnimationFrame(loop);
+}
+
 function loop()
 {
-  // draw state
-  // get input
-  // update state
-  // return new state
-
   requestAnimationFrame(loop);
 
   setPlayerDirection();
@@ -32,6 +42,14 @@ function loop()
   ctx.setTransform(1, 0, 0, 1, -windowX, -windowY);
   render();
 
+}
+
+function processMouseClick(e)
+{
+  const clickGridPositionX = Math.floor((e.clientX + windowX) / CELL_WIDTH);
+  const clickGridPositionY = Math.floor((e.clientY + windowY) / CELL_HEIGHT)
+
+  linkInfoParent.innerHTML = maze[clickGridPositionX][clickGridPositionY];
 }
 
 function setPlayerDirection()
@@ -50,13 +68,13 @@ function setPlayerDirection()
 
   // wall hit collision
 
-  // if (directionX < 0 && (maze[Math.floor(newX)][Math.floor(playerY)] === "wall"))
+  // if (directionX < 0 && (maze[Math.floor(newY)][Math.floor(newX - 1)] === "wall"))
   //   newX = playerX;
-  // if (directionX > 0 && (maze[Math.ceil(newX)][Math.floor(playerY)] === "wall"))
+  // if (directionX > 0 && (maze[Math.floor(newY)][Math.floor(newX) + 1] === "wall"))
   //   newX = playerX;
-  // if (directionY < 0 && maze[Math.floor(playerX)][Math.floor(newY)] === "wall")
+  // if (directionY < 0 && maze[Math.floor(newY - 1)][Math.floor(newX)] === "wall")
   //   newY = playerY;
-  // if (directionY > 0 && maze[Math.floor(playerX)][Math.ceil(newY)] === "wall")
+  // if (directionY > 0 && maze[Math.floor(newY + 1)][Math.floor(newX)] === "wall")
   //   newY = playerY;
 
   playerX = newX;
@@ -64,28 +82,14 @@ function setPlayerDirection()
 
   windowX = playerX * CELL_WIDTH + CELL_WIDTH / 2 - WINDOW_WIDTH / 2;
   windowY = playerY * CELL_HEIGHT + CELL_HEIGHT / 2 - WINDOW_HEIGHT / 2;
-
-  console.log(windowX, windowY);
-}
-
-async function start()
-{
-  addEventListener("keydown", e => keyStatus[e.key] = true);
-  addEventListener("keyup", e => keyStatus[e.key] = false);
-  requestAnimationFrame(loop);
-
-  // const articleProperties = await getArticleProperties("bassoon");
-  // const mazeProperties = generateMazeProperties(articleProperties);
-  const mazeProperties = {size: 20, simplicity: 0.6}
-  setupMaze(mazeProperties);
-  requestAnimationFrame(loop);
 }
 
 function generateMazeProperties(articleProperties)
 {
   return {
     size: articleProperties.wordCount / 100,
-    simplicity: 1 / (articleProperties.wordCount / 3000)
+    simplicity: 1 / (articleProperties.wordCount / 3000),
+    links: articleProperties.links.slice(0, Math.min(articleProperties.links.length / 100, 1)),
   }
 }
 
@@ -96,7 +100,11 @@ function setupMaze(properties)
     row.map(cell => cell.blocked ? "wall" : "space")
   )
   openUpMaze(properties.simplicity);
-  createEntrance();
+
+  const usableBorderTiles = getUsableBorderTiles();
+
+  createEntrance(usableBorderTiles);
+  createExits(properties.links, usableBorderTiles);
 }
 
 function openUpMaze(simplicity)
@@ -114,23 +122,66 @@ function openUpMaze(simplicity)
   }
 }
 
-function createEntrance(side = -1)
+function getUsableBorderTiles()
 {
-  if (side === -1)
-  {
-    const startTile = Math.floor((Math.random() * (maze.length - 1)));
+  const usableBorderTiles = []
 
-    for (let i = 0 ; i < maze.length ; i++)
-    {
-      const currentTile = (startTile + i) % (maze.length - 1);
-      if (maze[currentTile][0] === "wall" && maze[currentTile][1] === "space")
-      {
-        maze[currentTile][0] = "entrance"
-        playerX = currentTile;
-        playerY = 0;
-        return;
-      }
-    }
+  // top
+  for (let i = 0 ; i < maze.length ; i++)
+  {
+    if (maze[i][0] === "wall" && maze[i][1] === "space")
+        usableBorderTiles.push({x: i, y: 0});
+  }
+
+  // bottom
+  for (let i = 0 ; i < maze.length ; i++)
+  {
+    if (maze[i][maze.length - 1] === "wall" && maze[i][maze.length - 2] === "space")
+      usableBorderTiles.push({x: i, y: maze.length -1});
+  }
+
+  // left, minus top and bottom
+  for (let i = 1 ; i < maze.length -1  ; i++)
+  {
+    if (maze[0][i] === "wall" && maze[1][i] === "space")
+      usableBorderTiles.push({x: 0, y: i});
+  }
+
+  //right, minus top and bottom
+  for (let i = 1 ; i < maze.length -1  ; i++)
+  {
+    if (maze[maze.length - 1][i] === "wall" && maze[maze.length - 2][i] === "space")
+      usableBorderTiles.push({x: maze.length - 1, y: i});
+  }
+
+  return usableBorderTiles;
+}
+
+function createEntrance(usableBorderTiles)
+{
+  const index = Math.max(0, Math.floor(Math.random() * usableBorderTiles.length - 1));
+
+  const pos = usableBorderTiles[index];
+
+  usableBorderTiles.splice(index, 1);
+
+  maze[pos.x][pos.y] = "entrance";
+
+  playerX = pos.x;
+  playerY = pos.y;
+}
+
+function createExits(links, usableBorderTiles)
+{
+  for (let i = 0 ; i < links.length ; i++)
+  {
+    const index = Math.max(0, Math.floor(Math.random() * usableBorderTiles.length - 1));
+
+    usableBorderTiles.splice(index, 1);
+
+    const pos = usableBorderTiles[index];
+
+    maze[pos.x][pos.y] = "exit";
   }
 }
 
@@ -142,8 +193,7 @@ function render()
 
 function renderPlayer()
 {
-  ctx.fillStyle = "red";
-  ctx.fillRect(playerX * CELL_WIDTH, playerY * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
+  renderCell(playerX, playerY, "red");
 }
 
 function renderMaze()
@@ -151,16 +201,20 @@ function renderMaze()
   for (let i = 0 ; i < maze.length ; i++) {
     for (let j = 0; j < maze[i].length; j++) {
       if (maze[i][j] === "wall")
-        renderWallCell(i, j);
+        renderCell(i, j, "green");
+      else if (maze[i][j] === "exit" && exitsAreOpen)
+      {
+        renderCell(i, j, "yellow");
+      }
     }
   }
+}
 
-  function renderWallCell(i, j)
-  {
-    ctx.fillStyle = "green";
-    ctx.fillRect(i * CELL_WIDTH - 1, j * CELL_HEIGHT - 1, CELL_WIDTH + 1, CELL_HEIGHT + 1);
+function renderCell(i, j, color)
+{
+  ctx.fillStyle = color;
+  ctx.fillRect(i * CELL_WIDTH, j * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
 
-  }
 }
 
 export default function main() {}
