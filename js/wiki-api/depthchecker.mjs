99ce9a1e0caa9a455501c661a2131afb24d3c532
pandandog <alexmacl@mac.mynetworksettings.com>
2025-10-15 07:14:31 -0400

    justincase

--- js/wiki-api/depthchecker.mjs
+++ js/wiki-api/depthchecker.mjs
@@ -42,29 +42,61 @@ function urlize(link){
     return "<a href=\"http://en.wikipedia.org/wiki/"+link+"\">"+link+"</a>";
 }
 
-export async function* checkArticleScore(name){
+export async function rcheckArticleScore(name){
+    let retval=""
     await pullinPagestats(name,2,name);
     let xcites=[...new Set(cites)];
-    yield `<p><h1>Results</h1>`
+    retval+=`<p><h1>Results</h1>`;
     for(let i=0;i<xcites.length;i++){
 	let str = "Article "+urlize(xcites[i][1])+" has citation needed for: "+xcites[i][0]+"<p>";
 	console.log(str);
-    yield (str);
+	retval+=str;
     }
     for(let i=0;i<clari.length;i++){
 	let str = "Article "+urlize(clari[i][1])+" has clarification needed for: "+clari[i][0]+"<p>";
 	console.log(str);
-    yield (str);
+	retval+=str;
     }
     for(let i=0;i<reds.length;i++){
 	let str = "Article "+urlize(reds[i][1])+" has RED LINK for: "+urlize(reds[i][0])+"<p>";
 	console.log(str);
-    yield (str);
+	retval+=str;
     }
     let ostr = "<p>Final score is "+reds.length+"/"+clari.length+"/"+xcites.length;
     console.log(ostr);
-    yield `Hello`;
 //    console.log(cites);
+    return retval
 }
 
-await checkArticleScore("List of New York City Designated Landmarks in Brooklyn");
+export async function ncheckArticleScore(name){
+    let retval=""
+    await pullinPagestats(name,2,name);
+    let xcites=[...new Set(cites)];
+    retval+=`<p><h1>Results</h1>`;
+    for(let i=0;i<xcites.length;i++){
+	let str = "Article "+urlize(xcites[i][1])+" has citation needed for: "+xcites[i][0]+"<p>";
+	console.log(str);
+	retval+=str;
+    }
+    for(let i=0;i<clari.length;i++){
+	let str = "Article "+urlize(clari[i][1])+" has clarification needed for: "+clari[i][0]+"<p>";
+	console.log(str);
+	retval+=str;
+    }
+    for(let i=0;i<reds.length;i++){
+	let str = "Article "+urlize(reds[i][1])+" has RED LINK for: "+urlize(reds[i][0])+"<p>";
+	console.log(str);
+	retval+=str;
+    }
+    let ostr = "<p>Final score is "+reds.length+"/"+clari.length+"/"+xcites.length;
+    console.log(ostr);
+//    console.log(cites);
+    return retval
+}
+
+export async function* checkArticleScore(name){
+    yield(rcheckArticleScore(name));
+}
+
+
+rcheckArticleScore("List of New York City Designated Landmarks in Brooklyn");
