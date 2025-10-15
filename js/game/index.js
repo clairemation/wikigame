d2a75fb987040491fd9f80a2c4e848f0c25908b2
claire <clairemation@gmail.com>
2025-10-15 18:52:57 -0400

    Blah

--- js/game/index.js
+++ js/game/index.js
@@ -1,5 +1,6 @@
 const nmg = require('node-maze-generator');
 const {getArticleProperties} = require('../wiki-api/midlevelmanager.mjs');
+import generateMaze from './maze-generator.js';
 
 const scoreParent = document.querySelector('#score');
 const roomTitleParent = document.querySelector('#roomtitle')
@@ -9,55 +10,95 @@ const ctx = document.querySelector('canvas').getContext('2d');
 const CELL_WIDTH = 60, CELL_HEIGHT = 60;
 const WINDOW_WIDTH = 800, WINDOW_HEIGHT = 800;
 
-const countdownTime = 0;
-let acquiredTreasures = [];
-let currentRoomAcquiredTreasures = [];
-let playerIsStillEntering = false;
-let entranceName = 'bassoon';
 let animationFrame;
-let maze;
-let score = 0;
-let title = 'bassoon';
-let directionX = 0, directionY = 0;
-let windowX = 0, windowY = 0;
-let speed = 0.05;
-let keyStatus = {}
-let playerGridX = 0, playerGridY = 0;
-let exitsAreOpen = false;
-let positionToLinkName = {}
+let keyStatus = {};
 
-const shouldPopulateTreasures = () => !acquiredTreasures.find(entry => entry.room === title)
+const viewState = {
+  windowX: 0,
+  windowY: 0,
+}
 
-startRoom();
+const shouldPopulateTreasures = gameState => !gameState.acquiredTreasures.find(entry => entry.room === title)
 
-scoreParent.addEventListener('click', e => alert(acquiredTreasures))
+scoreParent.addEventListener('click', e => alert(gameState.acquiredTreasures))
 
-async function startRoom()
+start();
+
+function start()
 {
-  const articleProperties = await getArticleProperties(title);
-  const mazeProperties = generateMazeProperties(articleProperties);
-  setupMaze(mazeProperties);
-  playerIsStillEntering = true;
+  const gameState = {
+    acquiredTreasures: [],
+    currentRoomAcquiredTreasures: [],
+    playerIsStillEntering: false,
+    entranceName: 'aerophone',
+    maze: [],
+    score: 0,
+    title: 'aerophone',
+    playerDirectionX: 0,
+    playerDirectionY: 0,
+    playerSpeed: 0.05,
+    playerGridX: 0,
+    playerGridY: 0,
+  }
+
+  Object.freeze(gameState);
+
+  startRoom(gameState);
+}
+
+function createNewGameState(gameState, newProperties)
+{
+  const newGameState = {...gameState, ...newProperties};
+  Object.freeze(newGameState);
+  return newGameState;
+}
+
+async function startRoom(gameState)
+{
+  const articleProperties = await getArticleProperties(gameState.title);
+  const mazeProperties = generateMazeProperties(gameState, articleProperties);
+  const maze = generateMaze(mazeProperties);
+  const playerIsStillEntering = true;
+
+  let x, y;
+  for (let i = 0; i < maze.length; i++) {
+    for (let j = 0; j < maze.length; j++) {
+      if (maze[i][j].type === "entrance") {
+        x = i;
+        y = j;
+        break;
+      }
+      if (x)
+      {
+        break;
+      }
+    }
+  }
+  if (!x)
+  {
+    console.error("Entrance not found");
+  }
+
+  const playerGridX = x, playerGridY = y;
+
+  const newGameState = createNewGameState(gameState, {maze, playerIsStillEntering, playerGridX, playerGridY});
+  animationFrame = requestAnimationFrame(() => loop(newGameState));
 
   addEventListener("keydown", onKeyDown);
   addEventListener("keyup", onKeyUp);
   addEventListener("mousedown", processMouseClick);
 
   roomTitleParent.innerText = mazeProperties.title;
-
-  animationFrame = requestAnimationFrame(loop);
 }
 
-function clear()
+function stopAndClear()
 {
   cancelAnimationFrame(animationFrame);
-  directionX = 0;
-  directionY = 0;
   keyStatus = {};
   removeEventListener("keydown", onKeyDown);
   removeEventListener("keyup", onKeyUp);
   removeEventListener("mousedown", processMouseClick);
-  ctx.clearRect(windowX, windowY, WINDOW_WIDTH, WINDOW_HEIGHT);
+  ctx.clearRect(viewState.windowX, viewState.windowY, WINDOW_WIDTH, WINDOW_HEIGHT);
   ctx.setTransform(1, 0, 0, 1, 0, 0);
 }
 
@@ -71,42 +112,68 @@ function onKeyUp(e)
   keyStatus[e.key] = false;
 }
 
-function loop()
+function backtrackIntoEntrance(gameState) {
+  let gameStateUpdates = {}
+  gameStateUpdates.title = gameState.entranceName;
+  gameStateUpdates.entranceName = gameState.entranceName;
+
+  const newState = createNewGameState(gameState, gameStateUpdates);
+  stopAndClear();
+  startRoom(gameState);
+}
+
+function loop(gameState)
 {
-  animationFrame = requestAnimationFrame(loop);
+  const gameStateUpdates = {}
 
-  setPlayerPosition();
+  const playerGridPos = calcNewPlayerPosition(gameState);
+  gameStateUpdates.playerGridX = playerGridPos.x;
+  gameStateUpdates.playerGridY = playerGridPos.y;
 
-  const treasureAcquired = isPlayerOnTreasure();
+  const treasureAcquired = isPlayerOnTreasure(gameState);
   if (treasureAcquired)
   {
-    score++;
-    currentRoomAcquiredTreasures.push(treasureAcquired)
-    scoreParent.innerText = score;
-    maze[Math.floor(playerGridX)][Math.floor(playerGridY)].type = "space"
+    gameStateUpdates.score = gameState.score + 1;
+    gameStateUpdates.currentRoomAcquiredTreasures = [...gameState.currentRoomAcquiredTreasures, treasureAcquired];
+
+    // Yeah, this is not optimal for a huge maze, but we don't have a huge maze
+    // Also I don't think it even actually succeeds at being immutable at the cell level
+    const maze = {...gameState.maze};
+    maze[Math.floor(gameState.playerGridX)][Math.floor(gameState.playerGridY)].type = "space";
+    gameStateUpdates.maze = maze;
   }
 
-  if (isPlayerOnExit())
+
+  const {playerIsOnExit, exitTitle} = isPlayerOnExit(gameState);
+  if (playerIsOnExit)
   {
-    const treasuresFromRoom = {room: title, treasures: currentRoomAcquiredTreasures.splice(0)};
-    acquiredTreasures.push(treasuresFromRoom);
-    currentRoomAcquiredTreasures = [];
-    entranceName = title;
-    title = positionToLinkName[Math.floor(playerGridX)][Math.floor(playerGridY)];
-    clear();
-    startRoom();
-    console.log(acquiredTreasures);
+    if (gameState.currentRoomAcquiredTreasures.length > 0) {
+      gameStateUpdates.acquiredTreasures = [...gameState.acquiredTreasures, {room: gameState.title, treasures: gameState.currentRoomAcquiredTreasures}];
+    }
+
+    gameStateUpdates.currentRoomAcquiredTreasures = [];
+    gameStateUpdates.entranceName = gameState.title;
+    gameStateUpdates.title = exitTitle;
+
+    const newGameState = createNewGameState(gameState, gameStateUpdates);
+
+    stopAndClear();
+    startRoom(newGameState);
+    return;
   }
 
-  if (isPlayerOnEntrance())
+  let playerIsStillEntering = true;
+  if (isPlayerOnEntrance(gameState))
   {
-    if (!playerIsStillEntering)
+    if (!gameState.playerIsStillEntering)
     {
-      let temp = entranceName;
-      title = entranceName;
-      entranceName = temp;
-      clear();
-      startRoom();
+      gameStateUpdates.title = gameState.entranceName;
+      gameStateUpdates.entranceName = gameState.entranceName;
+
+      const newGameState = createNewGameState(gameState, gameStateUpdates);
+      stopAndClear();
+      startRoom(newGameState);
+      return;
     }
   }
   else
@@ -114,75 +181,89 @@ function loop()
     playerIsStillEntering = false;
   }
 
-  ctx.clearRect(windowX, windowY, WINDOW_WIDTH, WINDOW_HEIGHT);
+  ctx.clearRect(viewState.windowX, viewState.windowY, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-  ctx.setTransform(1, 0, 0, 1, -windowX, -windowY);
-  render();
+  const newGameState = createNewGameState(gameState,
+    {
+      playerIsStillEntering,
+      playerGridX: playerGridPos.x,
+      playerGridY: playerGridPos.y
+    });
 
+  ctx.setTransform(1, 0, 0, 1, -viewState.windowX, -viewState.windowY);
+  render(gameState);
+
+  animationFrame = requestAnimationFrame(() => loop(newGameState));
 }
 
-function isPlayerOnExit()
+function isPlayerOnExit(gameState)
 {
-  return maze[Math.floor(Math.max(playerGridX, 0))][Math.floor(Math.max(playerGridY, 0))].type === 'exit';
+  const cell = gameState.maze[Math.floor(Math.max(gameState.playerGridX, 0))][Math.floor(Math.max(gameState.playerGridY, 0))];
+  const playerIsOnExit = cell.type === "exit"
+  const exitTitle = cell.title;
+  return {playerIsOnExit, exitTitle};
 }
 
-function isPlayerOnTreasure()
+function isPlayerOnTreasure(gameState)
 {
   return (
-    maze[Math.floor(Math.max(playerGridX, 0))][Math.floor(Math.max(playerGridY, 0))].type === 'treasure' ?
-      maze[Math.floor(Math.max(playerGridX, 0))][Math.floor(Math.max(playerGridY, 0))].name : false
+    gameState.maze[Math.floor(Math.max(gameState.playerGridX, 0))][Math.floor(Math.max(gameState.playerGridY, 0))].type === 'treasure'
+      ? gameState.maze[Math.floor(Math.max(gameState.playerGridX, 0))][Math.floor(Math.max(gameState.playerGridY, 0))].name
+      : false
   );
 }
 
-function isPlayerOnEntrance()
+function isPlayerOnEntrance(gameState)
 {
-  return maze[Math.floor(Math.max(playerGridX, 0))][Math.floor(Math.max(playerGridY, 0))].type === 'entrance';
+  return gameState.maze[Math.floor(Math.max(gameState.playerGridX, 0))][Math.floor(Math.max(gameState.playerGridY, 0))].type === 'entrance';
 }
 
 function processMouseClick(e)
 {
-  try {
-    const clickGridPositionX = Math.floor((e.clientX - e.target.clientLeft + windowX) / CELL_WIDTH);
-    const clickGridPositionY = Math.floor((e.clientY - e.target.clientTop + windowY) / CELL_HEIGHT)
-
-    if (maze[clickGridPositionX][clickGridPositionY].type === "exit") {
-      linkInfoParent.innerText = positionToLinkName[clickGridPositionX][clickGridPositionY];
-    }
-    if (maze[clickGridPositionX][clickGridPositionY].type === "treasure") {
-      linkInfoParent.innerText = maze[clickGridPositionX][clickGridPositionY].name;
-    }
-    if (maze[clickGridPositionX][clickGridPositionY].type === "entrance") {
-      linkInfoParent.innerText = entranceName;
-    }
-  }
-  catch (e)
-  {
-    console.log(e)
-  }
+  // try {
+  //   const clickGridPositionX = Math.floor((e.clientX - e.target.clientLeft + windowX) / CELL_WIDTH);
+  //   const clickGridPositionY = Math.floor((e.clientY - e.target.clientTop + windowY) / CELL_HEIGHT)
+  //
+  //   if (maze[clickGridPositionX][clickGridPositionY].type === "exit") {
+  //     linkInfoParent.innerText = positionToLinkName[clickGridPositionX][clickGridPositionY];
+  //   }
+  //   if (maze[clickGridPositionX][clickGridPositionY].type === "treasure") {
+  //     linkInfoParent.innerText = maze[clickGridPositionX][clickGridPositionY].name;
+  //   }
+  //   if (maze[clickGridPositionX][clickGridPositionY].type === "entrance") {
+  //     linkInfoParent.innerText = entranceName;
+  //   }
+  // }
+  // catch (e)
+  // {
+  //   console.error(e)
+  // }
 }
 
-function setPlayerPosition()
+function calcNewPlayerPosition(gameState)
 {
-  directionX = 0;
-  directionY = 0;
-  if (keyStatus['w']) directionY--;
-  if (keyStatus['a']) directionX--;
-  if (keyStatus['s']) directionY++;
-  if (keyStatus['d']) directionX++;
+  const newPlayerGridPosition = {x: 0, y: 0};
 
-  let velocityX = directionX * speed;
-  let velocityY = directionY * speed;
+  let playerDirectionX = 0, playerDirectionY = 0;
+
+  if (keyStatus['w']) playerDirectionY--;
+  if (keyStatus['a']) playerDirectionX--;
+  if (keyStatus['s']) playerDirectionY++;
+  if (keyStatus['d']) playerDirectionX++;
+
+  let velocityX = playerDirectionX * gameState.playerSpeed;
+  let velocityY = playerDirectionY * gameState.playerSpeed;
 
   let checkPoints = {
-    upperLeft: {x: playerGridX + velocityX + 0.2, y: playerGridY +  velocityY + 0.2},
-    upperRight: {x: playerGridX + velocityX + 0.8, y: playerGridY + velocityY + 0.2},
-    lowerRight: {x: playerGridX + velocityX + 0.8, y: playerGridY + velocityY + 0.8},
-    lowerLeft: {x: playerGridX + velocityX + 0.2, y: playerGridY + velocityY + 0.8},
+    upperLeft: {x: gameState.playerGridX + velocityX + 0.2, y: gameState.playerGridY +  velocityY + 0.2},
+    upperRight: {x: gameState.playerGridX + velocityX + 0.8, y: gameState.playerGridY + velocityY + 0.2},
+    lowerRight: {x: gameState.playerGridX + velocityX + 0.8, y: gameState.playerGridY + velocityY + 0.8},
+    lowerLeft: {x: gameState.playerGridX + velocityX + 0.2, y: gameState.playerGridY + velocityY + 0.8},
   }
 
   if (velocityX < 0)
   {
-    if (checkForWall(checkPoints.upperLeft) || checkForWall(checkPoints.lowerLeft))
+    if (checkForWall(gameState, checkPoints.upperLeft) || checkForWall(gameState, checkPoints.lowerLeft))
     {
       velocityX = 0;
     }
@@ -190,7 +271,7 @@ function setPlayerPosition()
 
   else if (velocityX > 0)
   {
-    if (checkForWall(checkPoints.upperRight) || checkForWall(checkPoints.lowerRight))
+    if (checkForWall(gameState, checkPoints.upperRight) || checkForWall(gameState, checkPoints.lowerRight))
     {
       velocityX = 0;
     }
@@ -198,7 +279,7 @@ function setPlayerPosition()
 
   if (velocityY < 0)
   {
-    if (checkForWall(checkPoints.upperLeft) || checkForWall(checkPoints.upperRight))
+    if (checkForWall(gameState, checkPoints.upperLeft) || checkForWall(gameState, checkPoints.upperRight))
     {
       velocityY = 0;
     }
@@ -206,32 +287,32 @@ function setPlayerPosition()
 
   else if (velocityY > 0)
   {
-    if (checkForWall(checkPoints.lowerLeft) || checkForWall(checkPoints.lowerRight))
+    if (checkForWall(gameState, checkPoints.lowerLeft) || checkForWall(gameState, checkPoints.lowerRight))
     {
       velocityY = 0;
     }
   }
 
+  newPlayerGridPosition.x = gameState.playerGridX + velocityX;
+  newPlayerGridPosition.y = gameState.playerGridY + velocityY;
 
-  playerGridX += velocityX;
-  playerGridY += velocityY;
+  viewState.windowX = newPlayerGridPosition.x * CELL_WIDTH + CELL_WIDTH / 2 - WINDOW_WIDTH / 2;
+  viewState.windowY = newPlayerGridPosition.y * CELL_HEIGHT + CELL_HEIGHT / 2 - WINDOW_HEIGHT / 2;
 
-  windowX = playerGridX * CELL_WIDTH + CELL_WIDTH / 2 - WINDOW_WIDTH / 2;
-  windowY = playerGridY * CELL_HEIGHT + CELL_HEIGHT / 2 - WINDOW_HEIGHT / 2;
+  return newPlayerGridPosition;
 }
 
-function checkForWall(positionVector)
+function checkForWall(gameState, positionVector)
 {
-  console.log(positionVector);
-  return positionVector.x < 0 || positionVector.x >= maze.length
-    || positionVector.y < 0 || positionVector.y >= maze.length
-    || maze[Math.floor(positionVector.x)][Math.floor(positionVector.y)].type === "wall";
+  return positionVector.x < 0 || positionVector.x >= gameState.maze.length
+    || positionVector.y < 0 || positionVector.y >= gameState.maze.length
+    || gameState.maze[Math.floor(positionVector.x)][Math.floor(positionVector.y)].type === "wall";
 }
 
-function generateMazeProperties(articleProperties)
+function generateMazeProperties(gameState, articleProperties)
 {
   return {
-    title: title,
+    title: gameState.title,
     size: Math.max(articleProperties.wordCount / 400, 10),
     simplicity: 1 / (Math.ceil(articleProperties.links.length) / 70),
     links: articleProperties.links.slice(0, Math.max(articleProperties.links.length / 10, 1)),
@@ -239,144 +320,33 @@ function generateMazeProperties(articleProperties)
   }
 }
 
-function setupMaze(properties)
+function render(gameState)
 {
-  const generator = new nmg.generators.maze({}, {width: properties.size, height: properties.size});
-  maze = generator.data.grid.cells[0].map(row =>
-    row.map(cell => cell.blocked ? {type: "wall", x: cell.x, y: cell.y} : {type: "space", x: cell.x, y: cell.y})
-  )
-  openUpMaze(properties.simplicity);
-
-  const usableBorderTiles = getUsableBorderTiles();
-
-  createEntrance(usableBorderTiles);
-  createExits(properties.links, usableBorderTiles);
-  if (shouldPopulateTreasures())
-    createTreasures(properties.treasures);
+  renderMaze(gameState);
+  renderPlayer(gameState);
 }
 
-function openUpMaze(simplicity)
-{
-  for (let i = 0 ; i < maze.length ; i++)
-  {
-    for (let j = 0 ; j < maze[i].length ; j++)
-    {
-      const cell = maze[i][j];
-      if (cell.type === "wall" && i > 0 && j > 0 && i < maze.length - 1 && j < maze.length - 1 && Math.random() < simplicity)
-      {
-        maze[i][j].type = "space";
-      }
-    }
-  }
-}
-
-function getUsableBorderTiles()
-{
-  const usableBorderTiles = []
-
-  // top
-  for (let i = 0 ; i < maze.length ; i++)
-  {
-    if (maze[i][0].type === "wall" && maze[i][1].type === "space")
-        usableBorderTiles.push({x: i, y: 0});
-  }
-
-  // bottom
-  for (let i = 0 ; i < maze.length ; i++)
-  {
-    if (maze[i][maze.length - 1].type === "wall" && maze[i][maze.length - 2].type === "space")
-      usableBorderTiles.push({x: i, y: maze.length -1});
-  }
-
-  // left, minus top and bottom
-  for (let i = 1 ; i < maze.length -1  ; i++)
-  {
-    if (maze[0][i].type === "wall" && maze[1][i].type === "space")
-      usableBorderTiles.push({x: 0, y: i});
-  }
-
-  //right, minus top and bottom
-  for (let i = 1 ; i < maze.length -1  ; i++)
-  {
-    if (maze[maze.length - 1][i] === "wall".type && maze[maze.length - 2][i].type === "space")
-      usableBorderTiles.push({x: maze.length - 1, y: i});
-  }
-
-  return usableBorderTiles;
-}
-
-function createEntrance(usableBorderTiles)
-{
-  const index = Math.max(0, Math.floor(Math.random() * usableBorderTiles.length - 1));
-
-  const pos = usableBorderTiles[index];
-
-  usableBorderTiles.splice(index, 1);
-
-  maze[pos.x][pos.y].type = "entrance";
-
-  playerGridX = pos.x;
-  playerGridY = pos.y;
-}
-
-function createExits(links, usableBorderTiles)
-{
-  for (let i = 0 ; i < links.length ; i++)
-  {
-    const index = Math.max(0, Math.floor(Math.random() * usableBorderTiles.length - 1));
-
-    const pos = usableBorderTiles[index];
-
-    usableBorderTiles.splice(index, 1);
-
-    maze[pos.x][pos.y].type = "exit";
-
-    if (!positionToLinkName[pos.x])
-      positionToLinkName[pos.x] = []
-    positionToLinkName[pos.x][pos.y] = links[i];
-  }
-}
-
-function createTreasures(citesNeeded)
-{
-  const emptySpaces = maze.flat().filter(cell => cell.type === "space");
-
-  for (let i = 0 ; i < citesNeeded.length ; i++)
-  {
-    const rand = Math.floor((Math.random() * (emptySpaces.length - 1)));
-    emptySpaces[rand].type = "treasure";
-    emptySpaces[rand].name = citesNeeded[i];
-    emptySpaces.splice(rand, 1);
-  }
-}
-
-function render()
-{
-  renderMaze();
-  renderPlayer();
-}
-
-function renderPlayer()
+function renderPlayer(gameState)
 {
   // renderCell(playerGridX, playerGridY, "blue");
-  renderPlayerCell(playerGridX, playerGridY, "blue");
+  renderPlayerCell(gameState.playerGridX, gameState.playerGridY, "blue");
 }
 
-function renderMaze()
+function renderMaze(gameState)
 {
-  for (let i = 0 ; i < maze.length ; i++) {
-    for (let j = 0; j < maze[i].length; j++) {
-      if (maze[i][j].type === "wall")
+  for (let i = 0 ; i < gameState.maze.length ; i++) {
+    for (let j = 0; j < gameState.maze[i].length; j++) {
+      if (gameState.maze[i][j].type === "wall")
         renderCell(i, j, "green");
-      else if (maze[i][j].type === "exit" && !exitsAreOpen)
+      else if (gameState.maze[i][j].type === "exit" && !gameState.exitsAreOpen)
       {
         renderCell(i, j, "yellow");
       }
-      else if (maze[i][j].type === "entrance")
+      else if (gameState.maze[i][j].type === "entrance")
       {
         renderCell(i, j, "black");
       }
-      else if (maze[i][j].type === "treasure")
+      else if (gameState.maze[i][j].type === "treasure")
       {
         renderCell(i, j, "red");
       }
---
+++ js/game/maze-generator.js
@@ -0,0 +1,112 @@
+const nmg = require("node-maze-generator");
+
+export default function generateMaze(properties)
+{
+  const generator = new nmg.generators.maze({}, {width: properties.size, height: properties.size});
+  const maze = generator.data.grid.cells[0].map(row =>
+    row.map(cell => cell.blocked ? {type: "wall", x: cell.x, y: cell.y} : {type: "space", x: cell.x, y: cell.y})
+  )
+  openUpMazeInPlace(maze, properties.simplicity);
+
+  const usableBorderTiles = getUsableBorderTiles(maze);
+
+  createEntranceInPlace(maze, usableBorderTiles);
+  createExitsInPlace(maze, properties.links, usableBorderTiles);
+  // if (shouldPopulateTreasures(gameState))
+  createTreasuresInPlace(maze, properties.treasures);
+
+  return maze
+}
+
+function openUpMazeInPlace(maze, simplicity)
+{
+  for (let i = 0 ; i < maze.length ; i++)
+  {
+    for (let j = 0 ; j < maze[i].length ; j++)
+    {
+      const cell = maze[i][j];
+      if (cell.type === "wall" && i > 0 && j > 0 && i < maze.length - 1 && j < maze.length - 1 && Math.random() < simplicity)
+      {
+        maze[i][j].type = "space";
+      }
+    }
+  }
+}
+
+function getUsableBorderTiles(maze)
+{
+  const usableBorderTiles = []
+
+  // top
+  for (let i = 0 ; i < maze.length ; i++)
+  {
+    if (maze[i][0].type === "wall" && maze[i][1].type === "space")
+      usableBorderTiles.push({x: i, y: 0});
+  }
+
+  // bottom
+  for (let i = 0 ; i < maze.length ; i++)
+  {
+    if (maze[i][maze.length - 1].type === "wall" && maze[i][maze.length - 2].type === "space")
+      usableBorderTiles.push({x: i, y: maze.length -1});
+  }
+
+  // left, minus top and bottom
+  for (let i = 1 ; i < maze.length -1  ; i++)
+  {
+    if (maze[0][i].type === "wall" && maze[1][i].type === "space")
+      usableBorderTiles.push({x: 0, y: i});
+  }
+
+  //right, minus top and bottom
+  for (let i = 1 ; i < maze.length -1  ; i++)
+  {
+    if (maze[maze.length - 1][i] === "wall".type && maze[maze.length - 2][i].type === "space")
+      usableBorderTiles.push({x: maze.length - 1, y: i});
+  }
+
+  return usableBorderTiles;
+}
+
+function createEntranceInPlace(maze, usableBorderTiles)
+{
+  const index = Math.max(0, Math.floor(Math.random() * usableBorderTiles.length - 1));
+
+  const pos = usableBorderTiles[index];
+
+  usableBorderTiles.splice(index, 1);
+
+  maze[pos.x][pos.y].type = "entrance";
+}
+
+function createExitsInPlace(maze, links, usableBorderTiles)
+{
+  for (let i = 0 ; i < links.length ; i++)
+  {
+    const index = Math.max(0, Math.floor(Math.random() * usableBorderTiles.length - 1));
+
+    const pos = usableBorderTiles[index];
+
+    usableBorderTiles.splice(index, 1);
+
+    maze[pos.x][pos.y].type = "exit";
+    maze[pos.x][pos.y].title = links[i];
+
+    // if (!gameState.positionToLinkName[pos.x])
+    //   gameState.positionToLinkName[pos.x] = []
+    // gameState.positionToLinkName[pos.x][pos.y] = links[i];
+  }
+}
+
+function createTreasuresInPlace(maze, citesNeeded)
+{
+  const emptySpaces = maze.flat().filter(cell => cell.type === "space");
+
+  for (let i = 0 ; i < citesNeeded.length ; i++)
+  {
+    const rand = Math.floor((Math.random() * (emptySpaces.length - 1)));
+    emptySpaces[rand].type = "treasure";
+    emptySpaces[rand].name = citesNeeded[i];
+    emptySpaces.splice(rand, 1);
+  }
+}
